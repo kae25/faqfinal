892438cdc513f46cadfbd080bf075adabc204ba7
Kayla Evans <kae25@njit.edu>
2019-04-21 16:47:00 -0400

    Added answer test

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+    public function question()
+    {
+        return $this->belongsTo('App\Question');
+    }
 }
