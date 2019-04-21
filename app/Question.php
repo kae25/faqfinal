1e056d6fcd3b8f55fc2efa830cc1496938fbdb0c
Kayla Evans <kae25@njit.edu>
2019-04-21 17:01:47 -0400

    Answers model, migration, and seeder added

--- app/Question.php
+++ app/Question.php
@@ -13,4 +13,9 @@ class Question extends Model
         return $this->belongsTo('App\User');
     }
 
+    public function answers()
+    {
+        return $this->belongsTo('App\Answer');
+    }
+
 }
