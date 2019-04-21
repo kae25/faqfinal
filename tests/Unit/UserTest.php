1dcae3fa4744b07d69ea78f5df73eba677fbcd40
Kayla Evans <kae25@njit.edu>
2019-04-21 16:24:14 -0400

    Question model, migration, seeder added

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testQuestions()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->questions()->get()));
+    }
+}
