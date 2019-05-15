99a53e0a17c24cab7cb508d50f343f31f46f993d
Kayla Evans <kae25@njit.edu>
2019-05-15 13:54:55 -0400

    Updating welcome view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -44,6 +44,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script type=”text/javascript” src=”js/bootstrap-filestyle.min.js”> </script>
     <script src="/uploadimage.js"></script>
     <script>
 
